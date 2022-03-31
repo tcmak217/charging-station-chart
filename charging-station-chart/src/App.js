562c5c4ed997d0246d18e1e68263c24e4da471a0
LAPTOP-1M0IFO94\tcmak <tcmak217@outlook.com>
2022-04-01 01:02:17 +0800

    Add csv upload function

--- charging-station-chart/src/App.js
+++ charging-station-chart/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import CsvUpload from "./components/CsvUpload/CsvUpload";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <CsvUpload></CsvUpload>
+    </>
+  );
 }
 
 export default App;
---
+++ charging-station-chart/src/components/CsvUpload/CsvUpload.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./CsvUpload.css";
+
+function CsvUpload() {
+  return (
+    <>
+      <input type="file" id="myUploadFile" />
+      <div id="main"></div>
+    </>
+  );
+}
+
+export default CsvUpload;
