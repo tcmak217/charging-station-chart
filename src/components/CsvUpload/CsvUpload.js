8b9f7dc4d66576191138a9ea487352044e25f980
LAPTOP-1M0IFO94\tcmak <tcmak217@outlook.com>
2022-04-02 17:48:21 +0800

    Add upload .csv file and file read as text function

--- src/components/CsvUpload/CsvUpload.js
+++ src/components/CsvUpload/CsvUpload.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CsvUpload.css";
 
 function CsvUpload() {
+  const [file, setFile] = useState();
+
+  const fileReader = new FileReader();
+
+  const handleFileUpload = (e) => {
+    setFile(e.target.files[0]);
+    console.log("File uploaded");
+  };
+
+  const handleFileImport = (e) => {
+    e.preventDefault();
+    if (file) {
+      fileReader.onload = function (event) {
+        console.log(event.target.result);
+        const csvOutput = event.target.result;
+      };
+
+      fileReader.readAsText(file);
+    }
+  };
+
   return (
     <>
       <form>
-        <input type="file" id="myUploadFile" accept=".csv" />
+        <input
+          type="file"
+          id="myUploadFile"
+          accept=".csv"
+          onChange={handleFileUpload}
+        />
+        <button onClick={handleFileImport}>Import CSV file</button>
       </form>
     </>
   );
