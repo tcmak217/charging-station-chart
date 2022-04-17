8cac3323c2c7c4eccdb6e5206e2e210704351d94
LAPTOP-1M0IFO94\tcmak <tcmak217@outlook.com>
2022-04-17 22:00:23 +0800

    feat: add input limitation
    The date input is limited to the date before today. The time input of end date is limited to 24 hours after the start day.

--- src/components/DateRangePicker/DateRangePicker.js
+++ src/components/DateRangePicker/DateRangePicker.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import DatePicker from "react-datepicker";
+import { addDays, addHours } from "date-fns";
 import "react-datepicker/dist/react-datepicker.css";
 
 function DateRangePicker({
@@ -8,6 +9,22 @@ function DateRangePicker({
   handleSetStartDate,
   handleSetEndDate,
 }) {
+  const isPassedTime = (time) => {
+    const currentDate = new Date();
+    const selectedDate = new Date(time);
+
+    return currentDate.getTime() > selectedDate.getTime();
+  };
+
+  const isNextDay = (time) => {
+    const selectedDate = new Date(time);
+
+    return (
+      selectedDate.getTime() < addHours(startDate, 24).getTime() &&
+      selectedDate.getTime() > startDate
+    );
+  };
+
   return (
     <>
       Date range <br></br>
@@ -21,19 +38,23 @@ function DateRangePicker({
           console.log(start);
           handleSetStartDate(start);
         }}
+        filterTime={isPassedTime}
+        maxDate={new Date()}
         dateFormat="dd/MM/yyyy h:mm aa"
       />
       To
       <DatePicker
         startDate={startDate}
         endDate={endDate}
-        selected={endDate}
+        selected={endDate ? endDate : startDate}
         showTimeSelect
         onChange={(end) => {
           console.log(end);
           handleSetEndDate(end);
         }}
+        filterTime={isNextDay}
         minDate={startDate}
+        maxDate={startDate ? addDays(startDate, 1) : new Date()}
         dateFormat="dd/MM/yyyy h:mm aa"
       />
     </>
